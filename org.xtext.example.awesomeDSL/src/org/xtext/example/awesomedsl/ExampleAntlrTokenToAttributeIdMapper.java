4874465d97ea058da2dad08c07c6aa637fe122a3
Bernd Landauer <syrenio@gmail.com>
2016-06-04 22:07:01 +0200

    try something with XText and text styles

---
+++ org.xtext.example.awesomeDSL/src/org/xtext/example/awesomedsl/ExampleAntlrTokenToAttributeIdMapper.java
@@ -0,0 +1,25 @@
+package org.xtext.example.awesomedsl;
+
+import java.util.regex.Pattern;
+
+import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
+
+public class ExampleAntlrTokenToAttributeIdMapper extends AbstractAntlrTokenToAttributeIdMapper
+{
+    private static final Pattern QUOTED = Pattern.compile("(?:^'(\\w[^']*)'$)|(?:^\"(\\w[^\"]*)\")$", Pattern.MULTILINE);
+
+    @Override
+    protected String calculateId(String tokenName, int tokenType) {
+    	System.out.println(tokenName);
+        if(tokenName.equals("RULE_INT")) {
+            return ExampleHighlightingConfiguration.NUMBER_ID;
+        }
+        else if(tokenName.equals("RULE_STRING")){
+        	return ExampleHighlightingConfiguration.GREEN_ID;
+        }
+        else if(QUOTED.matcher(tokenName).matches()) {
+            return ExampleHighlightingConfiguration.KEYWORD_ID;
+        }
+        return ExampleHighlightingConfiguration.DEFAULT_ID;
+    }
+}
---
+++ org.xtext.example.awesomeDSL/src/org/xtext/example/awesomedsl/ExampleHighlightingConfiguration.java
@@ -0,0 +1,61 @@
+package org.xtext.example.awesomedsl;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
+import org.eclipse.xtext.ui.editor.utils.TextStyle;
+
+public class ExampleHighlightingConfiguration extends DefaultHighlightingConfiguration {
+
+    public static final String DEFAULT_ID = "default";
+    public static final String KEYWORD_ID = "keyword";
+    public static final String METHOD_ID = "method";
+    public static final String NUMBER_ID = "number";
+    public static final String HELLO_ID = "hello";
+    public static final String GREEN_ID = "green";
+
+    @Override
+    public void configure(IHighlightingConfigurationAcceptor acceptor) {
+        acceptor.acceptDefaultHighlighting(DEFAULT_ID, "Default", defaultTextStyle());
+        acceptor.acceptDefaultHighlighting(KEYWORD_ID, "Keyword", keywordTextStyle());
+        acceptor.acceptDefaultHighlighting(METHOD_ID, "Method", methodTextStyle());
+        acceptor.acceptDefaultHighlighting(NUMBER_ID, "Number", numberTextStyle());
+        acceptor.acceptDefaultHighlighting(HELLO_ID, "Hello", keywordTextStyle());
+        acceptor.acceptDefaultHighlighting(GREEN_ID, "Gello", greenTextStyle());
+    }
+
+    public TextStyle defaultTextStyle() {
+        TextStyle textStyle = new TextStyle();
+        textStyle.setColor(new RGB(0, 0, 0));
+        return textStyle;
+    }
+
+    public TextStyle greenTextStyle() {
+        TextStyle textStyle = defaultTextStyle().copy();
+        textStyle.setColor(new RGB(127, 255, 0));//127-255-0
+        textStyle.setStyle(SWT.BOLD);
+        return textStyle;
+    }
+    
+    public TextStyle keywordTextStyle() {
+        TextStyle textStyle = defaultTextStyle().copy();
+        textStyle.setColor(new RGB(255, 0, 0));//127-255-0
+        textStyle.setStyle(SWT.BOLD);
+        return textStyle;
+    }
+
+    protected TextStyle methodTextStyle() {
+        TextStyle textStyle = defaultTextStyle().copy();
+        textStyle.setColor(new RGB(85, 0, 127));
+        textStyle.setStyle(SWT.ITALIC);
+        return textStyle;
+    }
+
+    public TextStyle numberTextStyle() {
+        TextStyle textStyle = defaultTextStyle().copy();
+        textStyle.setColor(new RGB(127, 127, 127));
+        return textStyle;
+    }
+}
